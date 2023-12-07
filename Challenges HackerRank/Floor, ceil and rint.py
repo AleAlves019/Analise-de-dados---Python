258016a628e00d182bf4d9f06f27564440a9494d
Alexandre Alves Pinheiro <a230580@dac.unicamp.br>
2023-12-07 11:08:46 -0300

    modified:   Challenges HackerRank/Floor, ceil and rint.py

--- Challenges HackerRank/Floor, ceil and rint.py
+++ Challenges HackerRank/Floor, ceil and rint.py
@@ -4,6 +4,6 @@ np.set_printoptions(legacy = '1.13')
 entrada = np.array(input().split())
 entrada = [float(i) for i in entrada]
 
-print(np.floor(entrada))
-print(np.ceil(entrada))
-print(np.rint(entrada))
+print(np.floor(entrada))# Arredonda para o menor numero inteiro
+print(np.ceil(entrada))# arrendonda para o maior numero inteiro
+print(np.rint(entrada)) # arrendonda para o inteiro mais proximo
